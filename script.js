6465815dfe725ea758a0acbfc04dbdc0ecec5b55
Taynara <taynarasantossousa09@gmail.com>
2025-04-22 21:20:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let date = new Date()
+let horaAtual = date.getHours() /* 20 */
+let minutoAtual = date.getMinutes() /* 48 */
+let diaSemana = ["Dom","Seg","Ter","Qua","Qui","Sex","Sab"]
+let diaAtualSemana = diaSemana[date.getDay()] /* Ter */
+let diaAtualMes = date.getDate() /* 22 */
+let mesAno = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+letMesAtual = mesAno[date.getMonth()]
